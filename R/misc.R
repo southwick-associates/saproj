5ca9a04839aba0e8a1a61c36cce4554c8b765b3e
Daniel Kary <dankary@gmail.com>
2016-10-25 14:30:35 -0400

    fixed startup message text

--- R/misc.R
+++ R/misc.R
@@ -3,6 +3,6 @@
 # message to be displayed when package loads
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
-        "For an introduction to tablr:
+        "For an introduction to saproj:
 > vignette('saproj-intro')")
 }
